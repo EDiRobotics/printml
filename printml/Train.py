55670562aba462ab543ce9ce2f42b3ab178c519f
StarCycle <zhuohengli@foxmail.com>
2024-11-09 21:33:59 +0800

    add accelerate

--- printml/Train.py
+++ printml/Train.py
@@ -2,29 +2,102 @@
 A train loop
 """
 
+import os
+from pathlib import Path
 import torch
+from accelerate import Accelerator
+from accelerate.utils import DistributedDataParallelKwargs
 from printml.DeformDataset import DeformDataset
 from printml.UNet import DeformationPredictor
 
-dataset = DeformDataset('fake_deform_data.hdf5')
-dataloader = torch.utils.data.DataLoader(
-    dataset, 
-    batch_size=4,
-)
-predictor = DeformationPredictor(
-    num_levels=4, 
-    head_dim=64, 
-    n_heads=8,
-)
-optimizer = torch.optim.Adam(
-    predictor.net.parameters(),
-    lr=1e-4,
-)
+if __name__ == '__main__':
+    
+    # Saving path
+    save_path = Path('./Save/')
+    save_path.mkdir(parents=True, exist_ok=True)
 
-for epoch in range(100):
-    for batch in dataloader:
-        loss = predictor.compute_loss(batch)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        print(loss)
+    # Dataset
+    file_name = 'fake_deform_data.hdf5'
+    dataset_path = Path('/root/autodl-tmp/printml/printml') / file_name
+    bs_per_gpu = 64
+    workers_per_gpu = 4
+    cache_ratio = 2
+
+    # Network
+    num_levels = 4
+    head_dim = 64
+    n_heads = 8
+
+    # Training
+    num_training_epochs = 50
+    save_interval = 50
+    load_epoch_id = 0
+    gradient_accumulation_steps = 1
+    lr_max = 1e-4
+    warmup_steps = 5
+    weight_decay = 1e-4
+    max_grad_norm = 10
+    print_interval = 10
+    do_watch_parameters = False
+
+    # Preparation
+    kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+    acc = Accelerator(
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        log_with="wandb",
+        kwargs_handlers=[kwargs],
+    )
+    device = acc.device
+    dataset = DeformDataset(str(dataset_path))
+    loader = torch.utils.data.DataLoader(
+        dataset, 
+        batch_size=bs_per_gpu,
+        num_workers=workers_per_gpu,
+        prefetch_factor=cache_ratio,
+    )
+    predictor = DeformationPredictor(
+        num_levels=num_levels,
+        head_dim=head_dim,
+        n_heads=n_heads,
+        device=device,
+    )
+    predictor.load_pretrained(acc, save_path, load_epoch_id)
+    predictor.load_wandb(acc, save_path, do_watch_parameters, save_interval)
+    optimizer = torch.optim.Adam(
+        predictor.net.parameters(),
+        lr=lr_max,
+    )
+    predictor.net, optimizer, loader = acc.prepare(
+        predictor.net, 
+        optimizer, 
+        loader, 
+        device_placement=[True, True, True],
+    )
+
+    # Training loop
+    for epoch in range(num_training_epochs):
+        total_loss = 0
+        for batch in loader:
+            with acc.accumulate(predictor.net):
+                predictor.net.train()
+                loss = predictor.compute_loss(batch)
+                optimizer.zero_grad()
+                acc.backward(loss)
+                optimizer.step()
+                total_loss += loss.item()
+
+        # Calculate average loss for the epoch
+        average_loss = total_loss / len(loader)
+        print(f"Epoch {epoch+1}, Average Loss: {average_loss}")
+
+        # Log the average loss to wandb
+        acc.log({"Average Loss": average_loss})
+
+        # Save a checkpoint every "save_interval" epochs
+        if (epoch + 1) % save_interval == 0:
+            checkpoint = {
+                'epoch': epoch + 1,
+                'model_state_dict': predictor.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+            }
+            torch.save(checkpoint, f"checkpoint_{epoch+1}.pth")
--- printml/UNet.py
+++ printml/UNet.py
@@ -1,6 +1,9 @@
+import os
+import json
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import wandb
 from printml.DeformDataset import HEIGHT, WIDTH
 
 TRAJ_DIM = 2
@@ -89,10 +92,40 @@ class UNet(nn.Module):
 
 class DeformationPredictor():
 
-    def __init__(self, num_levels, head_dim, n_heads):
+    def __init__(self, num_levels, head_dim, n_heads, device):
         self.net = UNet(IN_CHANNELS, OUT_CHANNELS, num_levels, head_dim, n_heads)
-        self.x_coords = torch.linspace(0, 1, steps=WIDTH).unsqueeze(0).repeat(HEIGHT, 1)
-        self.y_coords = torch.linspace(0, 1, steps=HEIGHT).unsqueeze(1).repeat(1, WIDTH)
+        self.x_coords = torch.linspace(0, 1, steps=WIDTH).unsqueeze(0).repeat(HEIGHT, 1).to(device)
+        self.y_coords = torch.linspace(0, 1, steps=HEIGHT).unsqueeze(1).repeat(1, WIDTH).to(device)
+    
+    def save_pretrained(self, acc, path, epoch_id):
+        acc.wait_for_everyone()
+        acc.save(acc.unwrap_model(self.net).state_dict(), path / f'net_{epoch_id}.pth')
+
+    def load_pretrained(self, acc, path, load_epoch_id):
+        if os.path.isfile(path / f'net_{load_epoch_id}.pth'):
+            ckpt = torch.load(path / f'net_{load_epoch_id}.pth', map_location='cpu', weights_only=True)
+            missing_keys, unexpected_keys = self.net.load_state_dict(ckpt, strict=False)
+            acc.print('load ', path / f'net_{load_epoch_id}.pth', '\nmissing ', missing_keys, '\nunexpected ', unexpected_keys)
+        else: 
+            acc.print(path / f'net_{load_epoch_id}.pth', 'does not exist. Initialize new checkpoint')
+
+    def load_wandb(self, acc, path, do_watch_parameters, save_interval):
+        if os.path.isfile(path / "wandb_id.json"):
+            run_id = json.load(open(path / "wandb_id.json", "r"))
+            acc.init_trackers(
+                project_name="printml", 
+                init_kwargs={"wandb": {"id": run_id, "resume": "allow"}}
+            )
+            if acc.is_main_process:
+                if do_watch_parameters:
+                    wandb.watch(self.net, log="all", log_freq=save_interval)
+        else: 
+            acc.init_trackers(project_name="printml")
+            if acc.is_main_process:
+                tracker = acc.get_tracker("wandb")
+                json.dump(tracker.run.id, open(path / "wandb_id.json", "w"))
+                if do_watch_parameters:
+                    wandb.watch(self.net, log="all", log_freq=save_interval)
     
     def compute_loss(self, batch):
         B = batch["temperature"].shape[0]
